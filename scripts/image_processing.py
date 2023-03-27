88f3d0b156feaaabc4899e9515045c0024633c95
manuelgitgomes <manuelgomes@ua.pt>
2023-03-27 11:53:14 +0100

    #4 Create interface using opencv

---
+++ scripts/image_processing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2 as cv
+
+def main():
+    cap = cv.VideoCapture(4) #TODO, verify if this value is correct in intel NUC
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Display the resulting frame
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
