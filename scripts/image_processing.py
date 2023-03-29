8bc65b2c7c889bedbc0a329acecc8b76bcc5769d
Joao Silva <joaofernando@ua.pt>
2023-03-29 17:33:24 +0100

    minor changes

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -4,10 +4,13 @@ import numpy as np
 import cv2 as cv
 
 def main():
-    cap = cv.VideoCapture(4) #TODO, verify if this value is correct in intel NUC
+    cap = cv.VideoCapture(0) #TODO, verify if this value is correct in intel NUC
+
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
+
+
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -15,13 +18,16 @@ def main():
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
+
         # Display the resulting frame
         cv.imshow('frame', frame)
         if cv.waitKey(1) == ord('q'):
             break
+
     # When everything done, release the capture
     cap.release()
     cv.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
+    
